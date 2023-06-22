59fb8fcbfa1fc004dc79797b11aaf11b7c1f4b7f
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-06-22 16:30:55 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const input = document.getElementById('menu-toggle');
 
+const open = document.getElementById('open');
+
+
+const close = document.getElementById('close');
+
+
 input.addEventListener('change', function(event) {
   if (input.checked) {
     alert("Olá, mundo!");
