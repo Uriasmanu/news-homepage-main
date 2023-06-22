6d300d30b44da7038798bc2471eb70e33bdcccfc
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-06-22 20:23:59 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const input = document.getElementById('menu-toggle');
 const open = document.getElementById('open');
 const close = document.getElementById('close');
-const container-navebar = document.getElementById('container-navebar');
+const container = document.getElementById('container-navebar');
 
 
 input.addEventListener('click', function(event) {
   if (input.checked) {
     open.style.display = 'none';
     close.style.display = 'block';
-  container-navebar.style.display = 'block';
+  container.style.display = 'block';
   }
   
   else{
     open.style.display = 'block';
     close.style.display = 'none';
-container-navebar.style.display = 'none';
+container.style.display = 'none';
 }
 
 });
