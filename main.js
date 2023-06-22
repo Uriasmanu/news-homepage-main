3420ced366e3b78a047766ba5b305428ba702148
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-06-22 14:58:19 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const input = document.getElementById('menu-toggle');
 
 input.addEventListener('click', function(event) {
-  if (event.target.tagName !== 'LI') {
+  if (event.target.tagName !== 'li') {
     alert("Olá, mundo!");
   }
 });
