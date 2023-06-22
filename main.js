e050f0d0216d62e96a4492de6dc774945c766048
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-06-22 15:14:24 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const label = document.querySelector('menu-toggle-label');
+const label = document.querySelector('menu-toggle');
 
 label.addEventListener('click', function(event) {
   if (event.target === input) {
