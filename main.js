a07822188e84877d6394a4bcae94ee872980dddb
Manoela Urias <maanoelaurias@gmail.com~>
2023-06-23 10:34:37 -0300

    ,

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ input.addEventListener('click', function(event) {
   if (input.checked) {
     open.style.display = 'none';
     close.style.display = 'block';
-    menu.style.display = 'block';
+    menu.style.display = 'none';
   }
   
   else{
     open.style.display = 'block';
     close.style.display = 'none';
-    menu.style.display = 'none';
+    menu.style.display = 'block';
 }
 
 });
