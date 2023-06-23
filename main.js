8e3ad75388cac0089c7273523e9bc67217441ad5
Manoela Urias <maanoelaurias@gmail.com~>
2023-06-23 10:31:25 -0300

    .

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const input = document.getElementById('menu-toggle');
 const open = document.getElementById('open');
 const close = document.getElementById('close');
-const container = document.getElementById('container-navebar');
+const menu = document.getElementById('container');
 
 
 input.addEventListener('click', function(event) {
   if (input.checked) {
     open.style.display = 'none';
     close.style.display = 'block';
-  container.style.display = 'block';
+    menu.style.display = 'block';
   }
   
   else{
     open.style.display = 'block';
     close.style.display = 'none';
-container.style.display = 'none';
+    menu.style.display = 'none';
 }
 
 });
